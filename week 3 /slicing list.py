292b12e0be4d500f2436a20e3c7cd9a572f46c42
dileepnayak23 <gugulothudileep898@gmail.com>
2025-04-17 11:18:12 +0530

    Create slicing list.py

---
+++ week 3 /slicing list.py
@@ -0,0 +1,278 @@
+'''
+my_list = [1, 5, 10, 15, 20]
+print(my_list)
+
+print([True, False, False, "1"])
+
+'''
+
+'''
+my_list = list(["Lists", "are", "useful!"])
+print(my_list)
+'''
+'''
+my_list = [num for num in range (1,6)]
+print(my_list)
+'''
+
+'''
+my_list = [num for num in range (1,6)]
+print(my_list)
+
+element = my_list[2]
+print(element)
+'''
+
+'''
+
+my_list = [num for num in range (1,6)]
+my_list[start:end]
+
+my_list = [1, 5, 10, 15, 20, 25, 30, 35, 40]
+my_new_list = [2:5]
+
+print(my_new_list)
+
+'''
+
+'''
+my_list = [num for num in range (1,6)]
+
+last_element = my_list[-1]
+print(last_element)
+'''
+
+'''
+my_list = [num for num in range (1,6)]
+
+try:
+    element = my_list[10]
+except IndexError:
+    print("Index is out of range!")
+
+'''
+
+'''
+my_list = [12, 19, 26, 23]
+my_list.append(30)
+print(my_list)
+'''
+'''
+python_datatypes = ["lists"]
+python_datatypes.extend(["tuples", "sets"])
+print(python_datatypes)
+'''
+
+'''
+
+blue_shades = ['navy', 'sky blue', 'saphire', 'powder blue', 'teal', 'turquoise']
+blue_shades[2] = 'sapphire'
+
+print(blue_shades)
+'''
+
+'''
+blue_shades = ['navy', 'sky blue', 'sapphire', 'powder blue', 'teal', 'turquoise']
+blue_shades[0:2] = ['denim', 'aqua']
+
+print(blue_shades)
+'''
+
+'''
+blue_shades = ['navy', 'sky blue', 'sapphire', 'powder blue', 'teal', 'turquoise']
+blue_shades[0:1] = ['denim', 'aqua']
+print(blue_shades)
+'''
+
+'''
+
+blue_shades = ['navy', 'sky blue', 'sapphire', 'powder blue', 'teal', 'turquoise']
+blue_shades[0:4] = ['denim', 'aqua']
+print(blue_shades)
+'''
+
+'''
+blue_shades = ['navy', 'sky blue', 'sapphire', 'powder blue', 'teal', 'turquoise']
+
+blue_shades = ['cobalt', 'azure', 'ice blue']
+print(blue_shades)
+'''
+
+'''
+space_movies = ['Interstellar', 'Gravity', 'The Martian', 'Apollo 13', 'Star Wars']
+print(space_movies)
+
+del space_movies[2]
+print(space_movies)
+'''
+
+'''
+space_movies = ['Interstellar', 'Gravity', 'The Martian', 'Apollo 13', 'Star Wars']
+
+del space_movies[1:3]
+print(space_movies)
+'''
+
+'''
+space_movies = ['Interstellar', 'Gravity', 'The Martian', 'Apollo 13', 'Star Wars']
+print(space_movies)
+
+space_movies.remove('Gravity')
+print(space_movies)
+'''
+
+'''
+space_movies = ['Interstellar', 'Gravity', 'The Martian', 'Apollo 13', 'Star Wars']
+space_movies.clear()
+print(space_movies)
+'''
+
+'''
+space_movies = ['Interstellar', 'Gravity', 'The Martian', 'Apollo 13', 'Star Wars']
+
+removed_movie = space_movies.pop(2)
+print("The removed movie is = " + removed_movie)
+print(space_movies)
+'''
+
+'''
+
+'''
+my_list = [1, 5, 10, 15, 20]
+print(my_list)
+
+print([True, False, False, "1"])
+
+'''
+
+'''
+my_list = list(["Lists", "are", "useful!"])
+print(my_list)
+'''
+'''
+my_list = [num for num in range (1,6)]
+print(my_list)
+'''
+
+'''
+my_list = [num for num in range (1,6)]
+print(my_list)
+
+element = my_list[2]
+print(element)
+'''
+
+'''
+
+my_list = [num for num in range (1,6)]
+my_list[start:end]
+
+my_list = [1, 5, 10, 15, 20, 25, 30, 35, 40]
+my_new_list = [2:5]
+
+print(my_new_list)
+
+'''
+
+'''
+my_list = [num for num in range (1,6)]
+
+last_element = my_list[-1]
+print(last_element)
+'''
+
+'''
+my_list = [num for num in range (1,6)]
+
+try:
+    element = my_list[10]
+except IndexError:
+    print("Index is out of range!")
+
+'''
+
+'''
+my_list = [12, 19, 26, 23]
+my_list.append(30)
+print(my_list)
+'''
+'''
+python_datatypes = ["lists"]
+python_datatypes.extend(["tuples", "sets"])
+print(python_datatypes)
+'''
+
+'''
+
+blue_shades = ['navy', 'sky blue', 'saphire', 'powder blue', 'teal', 'turquoise']
+blue_shades[2] = 'sapphire'
+
+print(blue_shades)
+'''
+
+'''
+blue_shades = ['navy', 'sky blue', 'sapphire', 'powder blue', 'teal', 'turquoise']
+blue_shades[0:2] = ['denim', 'aqua']
+
+print(blue_shades)
+'''
+
+'''
+blue_shades = ['navy', 'sky blue', 'sapphire', 'powder blue', 'teal', 'turquoise']
+blue_shades[0:1] = ['denim', 'aqua']
+print(blue_shades)
+'''
+
+'''
+
+blue_shades = ['navy', 'sky blue', 'sapphire', 'powder blue', 'teal', 'turquoise']
+blue_shades[0:4] = ['denim', 'aqua']
+print(blue_shades)
+'''
+
+'''
+blue_shades = ['navy', 'sky blue', 'sapphire', 'powder blue', 'teal', 'turquoise']
+
+blue_shades = ['cobalt', 'azure', 'ice blue']
+print(blue_shades)
+'''
+
+'''
+space_movies = ['Interstellar', 'Gravity', 'The Martian', 'Apollo 13', 'Star Wars']
+print(space_movies)
+
+del space_movies[2]
+print(space_movies)
+'''
+
+'''
+space_movies = ['Interstellar', 'Gravity', 'The Martian', 'Apollo 13', 'Star Wars']
+
+del space_movies[1:3]
+print(space_movies)
+'''
+
+'''
+space_movies = ['Interstellar', 'Gravity', 'The Martian', 'Apollo 13', 'Star Wars']
+print(space_movies)
+
+space_movies.remove('Gravity')
+print(space_movies)
+'''
+
+'''
+space_movies = ['Interstellar', 'Gravity', 'The Martian', 'Apollo 13', 'Star Wars']
+space_movies.clear()
+print(space_movies)
+'''
+
+'''
+space_movies = ['Interstellar', 'Gravity', 'The Martian', 'Apollo 13', 'Star Wars']
+
+removed_movie = space_movies.pop(2)
+print("The removed movie is = " + removed_movie)
+print(space_movies)
+'''
+
+'''
+
